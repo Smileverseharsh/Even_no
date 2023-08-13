0b1a2fb9a6d56768afca9e663d174307dc0788d7
Smileverseharsh <128120742+Smileverseharsh@users.noreply.github.com>
2023-08-14 00:09:11 +0530

    Add files via upload

---
+++ even_no.java
@@ -0,0 +1,23 @@
+package Apna.function;
+
+import java.util.Scanner;
+
+public class even_no {
+    public static  int prime(int n){
+    
+        if (n%2 == 0)
+         System.out.println("No. is prime");
+        else
+            System.out.println("Number is not prime");
+        return n;
+}
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+   
+    System.out.println("Enter No. to check prime");
+    int n= sc.nextInt();
+    prime(n);
+}
+}
+
+
